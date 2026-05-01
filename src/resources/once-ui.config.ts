7462e261c868584fb4cb3fe564517da2e944dc66
Vivan Kushal Heneger <144650614+vivankushal55@users.noreply.github.com>
2026-05-01 01:49:18 +0100

    Update baseURL and schema data for portfolio

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,14 +14,14 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://vivankushal-portfolio.vercel.app";
 
 const routes: RoutesConfig = {
   "/": true,
   "/about": true,
   "/work": true,
-  "/blog": true,
-  "/gallery": true,
+  "/blog": false,
+  "/gallery": false,
 };
 
 const display: DisplayConfig = {
@@ -187,17 +187,15 @@ const mailchimp: MailchimpConfig = {
 // default schema data
 const schema: SchemaConfig = {
   logo: "",
-  type: "Organization",
-  name: "Once UI",
+  type: "Person",
+  name: "Vivan Kushal Heneger",
   description: home.description,
-  email: "lorant@once-ui.com",
+  email: "vivankushal55@gmail.com",
 };
 
 // social links
 const sameAs: SameAsConfig = {
-  threads: "https://www.threads.com/@once_ui",
-  linkedin: "https://www.linkedin.com/company/once-ui/",
-  discord: "https://discord.com/invite/5EyAQ4eNdS",
+  linkedin: "https://www.linkedin.com/in/vivankushal55/",
 };
 
 // social sharing configuration for blog posts
